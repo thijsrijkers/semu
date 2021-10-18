9585d47ce7f3096a4c8b43c1190a3cd276be5ec9
thijsrijkers <thijsrijkers@live.nl>
2021-10-18 20:09:06 +0200

    +added some sources

--- SeMu.cpp
+++ SeMu.cpp
@@ -1,6 +1,5 @@
 // SeMu.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include "pch.h"
 #include <iostream>
 
---
+++ cpu.cpp
@@ -0,0 +1,8 @@
+// ARM Cortex-A57
+//https://en.wikipedia.org/wiki/Comparison_of_ARMv8-A_cores
+//https://en.wikipedia.org/wiki/Instruction_pipelining
+//
+
+#include "pch.h"
+#include <iostream>
+
