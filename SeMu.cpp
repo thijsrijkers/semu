54667fa416f8733d8fb068c10cae2ea7b7bf9b84
thijsrijkers <t.rijkers@provrex.nl>
2021-10-18 13:24:35 +0200

    + Imported project

---
+++ SeMu.cpp
@@ -0,0 +1,11 @@
+// SeMu.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "SeMu\n";
+}
+
